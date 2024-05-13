ef7644cda32244ffd1ec2fdd87f7b76efcb6ea7b
Eric C. Weig <eweig@email.uky.edu>
2024-05-13 08:38:21 -0400

    Update globalConfig.js

--- js/config/global/globalConfig.js
+++ js/config/global/globalConfig.js
@@ -1,1 +1,30 @@
-
+export default {
+	DangerTitleText: "0 - UK ALERT - DELAYED OPENING",
+	DangerMsgText:
+		"URGENT: University of Kentucky campus operations on a 2-hour delay. All library facilities will open at 10 am, today, Friday, Jan 19.",
+	DangerMsgToggle: "on",
+	DangerMsgBackground: "#c12c2b",
+	DangerMsgColor: "#fff",
+	DangerLinkColor: "#fff",
+	WarningTitleText: "0 - UK ALERT - EARLY CANCELLATION",
+	WarningMsgText:
+		"We are currently experiencing technical difficulties with some online databases.  We are working to resolve this issue as soon as possible.  Thank you for your patience. ",
+	WarningMsgToggle: "off",
+	WarningMsgBackground: "#FFDC00",
+	WarningMsgColor: "#000",
+	WarningLinkColor: "#0033A0",
+	AnnouncementTitleText: "0 - UK ALERT - EARLY CANCELLATION",
+	AnnouncementMsgText:
+		"We are currently experiencing technical difficulties with some online databases.  We are working to resolve this issue as soon as possible.  Thank you for your patience. ",
+	AnnouncementMsgToggle: "off",
+	AnnouncementMsgBackground: "#4cbcc0",
+	AnnouncementMsgColor: "#000",
+	AnnouncementLinkColor: "#0033A0",
+	SurveyTitleText: "Help us improve library services, spaces, and collections!",
+	SurveyMsgText: "Take the ",
+	SurveyMsgLinkLabel: "LibQUAL+ survey",
+	SurveyMsgLinkURL: "https://www.libqual.org/survey/RWSLDQ72YU886X8HRA",
+	SurveyMsgToggle: "on",
+	SurveyMsgBackground: "#sdf",
+	SurveyMsgColor: "#000",
+};
