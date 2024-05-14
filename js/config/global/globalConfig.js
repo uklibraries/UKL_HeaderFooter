9f4ca46a3facbac2ba3dec323dcd1d6a15b484ce
Neal Powers <neal.grindstaff@gmail.com>
2024-05-14 08:29:23 -0400

    clean up and announcement fix

--- js/config/global/globalConfig.js
+++ js/config/global/globalConfig.js
@@ -2,7 +2,7 @@ export default {
 	DangerTitleText: "0 - UK ALERT - DELAYED OPENING",
 	DangerMsgText:
 		"URGENT: University of Kentucky campus operations on a 2-hour delay. All library facilities will open at 10 am, today, Friday, Jan 19.",
-	DangerMsgToggle: "on",
+	DangerMsgToggle: "off",
 	DangerMsgBackground: "#c12c2b",
 	DangerMsgColor: "#fff",
 	DangerLinkColor: "#fff",
@@ -17,7 +17,7 @@ export default {
 	AnnouncementMsgText:
 		"We are currently experiencing technical difficulties with some online databases.  We are working to resolve this issue as soon as possible.  Thank you for your patience. ",
 	AnnouncementMsgToggle: "off",
-	AnnouncementMsgBackground: "#4cbcc0",
+	AnnouncementMsgBackground: "#a1d3ed",
 	AnnouncementMsgColor: "#000",
 	AnnouncementLinkColor: "#0033A0",
 	SurveyTitleText: "Help us improve library services, spaces, and collections!",
@@ -25,6 +25,6 @@ export default {
 	SurveyMsgLinkLabel: "LibQUAL+ survey",
 	SurveyMsgLinkURL: "https://www.libqual.org/survey/RWSLDQ72YU886X8HRA",
 	SurveyMsgToggle: "on",
-	SurveyMsgBackground: "#sdf",
+	SurveyMsgBackground: "#4CBCC0",
 	SurveyMsgColor: "#000",
 };
