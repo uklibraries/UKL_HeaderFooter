961dd8e0b9e3f0924bafb2889fcd923a01f3320c
Eric C. Weig <eweig@email.uky.edu>
2024-05-14 09:05:32 -0400

    Update globalConfig.js
    Adding in changes from neal and test site.

--- js/config/global/globalConfig.js
+++ js/config/global/globalConfig.js
@@ -2,7 +2,7 @@ export default {
 	DangerTitleText: "0 - UK ALERT - DELAYED OPENING",
 	DangerMsgText:
 		"URGENT: University of Kentucky campus operations on a 2-hour delay. All library facilities will open at 10 am, today, Friday, Jan 19.",
-	DangerMsgToggle: "on",
+	DangerMsgToggle: "off",
 	DangerMsgBackground: "#c12c2b",
 	DangerMsgColor: "#fff",
 	DangerLinkColor: "#fff",
@@ -16,8 +16,8 @@ export default {
 	AnnouncementTitleText: "0 - UK ALERT - EARLY CANCELLATION",
 	AnnouncementMsgText:
 		"We are currently experiencing technical difficulties with some online databases.  We are working to resolve this issue as soon as possible.  Thank you for your patience. ",
-	AnnouncementMsgToggle: "off",
-	AnnouncementMsgBackground: "#4cbcc0",
+	AnnouncementMsgToggle: "on",
+	AnnouncementMsgBackground: "#a1d3ed",
 	AnnouncementMsgColor: "#000",
 	AnnouncementLinkColor: "#0033A0",
 	SurveyTitleText: "Help us improve library services, spaces, and collections!",
@@ -25,6 +25,6 @@ export default {
 	SurveyMsgLinkLabel: "LibQUAL+ survey",
 	SurveyMsgLinkURL: "https://www.libqual.org/survey/RWSLDQ72YU886X8HRA",
 	SurveyMsgToggle: "on",
-	SurveyMsgBackground: "#sdf",
+	SurveyMsgBackground: "#4CBCC0",
 	SurveyMsgColor: "#000",
 };
