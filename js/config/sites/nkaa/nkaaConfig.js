3a143795422a778e178149b0809f350ad23150db
Eric C. Weig <eweig@email.uky.edu>
2024-09-26 14:11:00 -0400

    Update nkaaConfig.js with current settings for production

--- js/config/sites/nkaa/nkaaConfig.js
+++ js/config/sites/nkaa/nkaaConfig.js
@@ -1,83 +1,16 @@
 export default {
-	hdr_simple: 0,
-	include: 1,
-	hdr_srch_include: 1,
+	hdr_simple: 1,
+	include: 0,
+	hdr_srch_include: 0,
 	hdr_width: 80,
-	hdr_alert_include: 1,
-	hdr_alert_title: "Unexpected Service Disruption",
+	hdr_alert_include: 0,
+	hdr_alert_title: "Scheduled Service Interruption",
 	hdr_alert_msg:
-		"We are currently working to resolve an authentication issue affect EBSCO databases.",
-	hdr_alert_url_label: "More information",
-	hdr_alert_url: "https://google.com",
-	home_label: "NKAA",
-	home_url: "https://google.com",
+		"The NKAA database will be offline for scheduled maintenance on YYYY-MM-DD from 0:00 - 0:00.",
+	hdr_alert_url_label: "",
+	hdr_alert_url: "",
+	home_label: "",
+	home_url: "",
 	custom_logo: false,
-	image_path: "./js/config/libcal/img.png",
-	bases: [
-		{
-			label: "home",
-		},
-		{
-			title: "About",
-			label: "link1",
-			children: [
-				{
-					title: "About Collections",
-					label: "link1Child1",
-					url: "https://linktolibrary.com/about/collections",
-				},
-				{
-					title: "About King",
-					label: "link1Child2",
-					url: "https://linktolibrary.com/about/King",
-				},
-				{
-					title: "About UK",
-					label: "link1Child3",
-					url: "https://linktolibrary.com/about/UK",
-				},
-			],
-		},
-		{
-			title: "Kitty cats",
-			label: "link2",
-			children: [
-				{
-					title: "About kats",
-					label: "link1Child1",
-					url: "https://linktolibrary.com/about/collec",
-				},
-				{
-					title: "About King",
-					label: "link1Child2",
-					url: "https://linktolibrary.com/about/King",
-				},
-				{
-					title: "About UK",
-					label: "link1Child3",
-					url: "https://linktolibrary.com/about/UK",
-				},
-			],
-		},
-		{
-			title: "Stuff 3",
-			label: "link3",
-			url: "https://google.com/",
-		},
-		{
-			title: "Stuff 4",
-			label: "link4",
-			url: "https://google.com/",
-		},
-		{
-			title: "Stuff",
-			label: "link5",
-			url: "https://google.com/",
-		},
-		{
-			title: "Research Guides",
-			label: "link6",
-			url: "https://libguides.uky",
-		},
-	],
+	image_path: "",
 };
