5b669eedf9d627ce75149ba8caad48d620d627d3
Eric C. Weig <eweig@email.uky.edu>
2024-12-02 13:54:35 -0500

    Create dsdguidesConfig.js
    Adding config for Digital Scholarship & Data libguides

---
+++ js/config/sites/dsdguides/dsdguidesConfig.js
@@ -0,0 +1,39 @@
+export default {
+	hdr_simple: 0,
+	include: 1,
+	hdr_srch_include: 1,
+	hdr_width: 115,
+	hdr_alert_include: 0,
+	hdr_alert_title: "Scheduled Service Disruption",
+	hdr_alert_msg:
+		"The site will be offline for scheduled maintenance on YYYY-MM-DD.",
+	hdr_alert_url_label: "More information",
+	hdr_alert_url: "https://libguides.uky.edu/home",
+	home_label: "Digital Scholarship & Data",
+	home_url: "https://libguides.uky.edu/dsd",
+	custom_logo: false,
+	image_path: "./js/config/sites/libguides/logo.png",
+	bases: [
+		{
+			label: "home",
+		},
+		{
+			title: "'How To' Guides",
+			label: "link1",
+			url: "https://libguides.uky.edu/How",
+		},
+		{
+			title: "Course Guides",
+			label: "link2",
+			url: "https://libguides.uky.edu/courseguides",
+		},
+		{
+			title: "Contact Us",
+			label: "link5",
+		},
+		{
+			title: "Login",
+			label: "link6",
+		},
+	],
+};
