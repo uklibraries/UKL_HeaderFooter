c8c75ffc3f5e763a000dc8e7716d9f6779880189
Eric C. Weig <eweig@email.uky.edu>
2025-12-01 16:08:51 -0500

    turn off survey and warning alert for fallback CDN

--- js/config/global/globalConfig.js
+++ js/config/global/globalConfig.js
@@ -9,7 +9,7 @@ export default {
 	warning_title_text: "0 - UK ALERT - EARLY CANCELLATION",
 	warning_msg_text:
 		"We are currently experiencing technical difficulties with some online databases.  We are working to resolve this issue as soon as possible.  Thank you for your patience. ",
-	warning_msg_toggle: "on",
+	warning_msg_toggle: "off",
 	warning_msg_background: "#FFDC00",
 	warning_msg_color: "#000",
 	warning_link_color: "#0033A0",
@@ -24,7 +24,8 @@ export default {
 	survey_msg_text: "Take the ",
 	survey_msg_link_label: "LibQUAL+ survey",
 	survey_msg_link_url: "https://www.libqual.org/survey/RWSLDQ72YU886X8HRA",
-	survey_msg_toggle: "on",
+	survey_msg_toggle: "off",
 	survey_msg_background: "#4CBCC0",
 	survey_msg_color: "#0033a0",
 };
+
